3efdd51419762f88848afa56524311688c520bcc
Robin <robin.fasseler@gmail.com>
2025-12-04 18:49:49 +0000

    lock boss room

---
+++ Assets/Scripts/Common/BossDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class BossDoor : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
